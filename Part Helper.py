69815604cb45ba3d5b2e35fc5cfeaf7b522e98ee
NezukoTheSpirit <83093671+NezukoTheSpirit@users.noreply.github.com>
2021-04-30 18:21:49 +0100

    Add files via upload
    PC Parts Advice (To Be Expanded) V1.0

---
+++ Part Helper.py
@@ -0,0 +1,26 @@
+Userinput = input("Welcome to Part Advice. \n 1 - Cases \n 2 - CPU Coolers")
+
+#Cases
+
+if Userinput == "1":
+    print("Cases selected")
+    print("High end cases: \n Phanteks P500A (Digital) \n Corsair 4000D Airflow \n Lian Li Lancool-II Mesh \n Fractal Design Meshify 2")
+    print("Mid range cases: \n Cooler Master NR600/NR400 \n Fractal Design Meshify C \n Aerocool Aero One Eclipse \n Lian Li Lancool 205/215 \n Metallic Gear Neo Air \n Phanteks P360A \n Phanteks P400A Digital")
+
+#CPU Coolers
+
+if Userinput == "2":
+    print("CPU Coolers selected")
+
+CPUCinput = input("1 - Liquid Coolers \n2 - Air Coolers")
+
+if CPUCinput == "1":
+    print("Disclaimer: Most CPUs won't need an AIO. Unless you are overclocking, I recommend getting an air cooler.")
+
+    print("Good Overall AIOs: \n Arctic Liquid Freezer II 240MM (Recommended) \n Lian-Li GALAHAD 240MM \n EK EK AIO 240MM")
+
+if CPUCinput == "2":
+    print("Good overall Air Coolers: \n Gelid Solutions Phantom Black \n Scythe Mugen 5 Rev. B \n SE-224-XT Basic (Recommended) \n Arctic Freezer 34 eSports \n Scythe FUMA 2")
+
+#Continue later
+        
